a6c70da1ed8560ed3706dac8eb868a712dcb2a43
ilyapc <ilya.komendantov@list.ru>
2026-01-08 12:44:18 +0300

    Button

--- app/src/main/java/ru/ikom/composesurfaceviewbug/MainActivity.kt
+++ app/src/main/java/ru/ikom/composesurfaceviewbug/MainActivity.kt
@@ -10,8 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -20,9 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import ru.ikom.composesurfaceviewbug.ui.theme.ComposeSurfaceViewBugTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
